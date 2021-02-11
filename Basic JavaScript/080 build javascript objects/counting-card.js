70b9bc09ee8c739189d161cf9312720712ad4a0c
Reza <rezaaghajani@protonmail.com>
2021-02-12 02:35:58 +0330

    Create counting-cards.js

---
+++ Basic JavaScript/080 build javascript objects/counting-card.js
@@ -0,0 +1,29 @@
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
